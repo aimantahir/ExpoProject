eb4ae866b7707f06da7bb9d49a2fb84286581eb4
aimantahir25 <aimantahir1225@gmail.com>
2023-01-09 01:59:23 +0500

    Update App.js
    Goals

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+  function goalInputHanlder(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+  function addGoalHandler() {
+    setCourseGoals((currentCourseGoals) => [...courseGoals, enteredGoalText]);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder='Your course goal'
+          onChangeText={goalInputHanlder}
+        />
+        <Button title='Add goal' onPress={addGoalHandler} />
+      </View>
+      <View style={styles.goalContainer}>
+        <Text>
+          {courseGoals.map((goal) => (
+            <Text key={goal}>{goal}{'\n'}</Text>
+          ))}
+        </Text>
+      </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#CCCCCC',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#CCCCCC',
+    width: '70%',
+    marginRight: 8,
+    padding: 8,
+  },
+  goalContainer: {
+    flex: 4,
   },
 });
+
+
+
+
+
+
+
+
+
+
+
+Jot something down
+
+
+
